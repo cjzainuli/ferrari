9544dd0f70ef456c3d8d58b7cc25c4f14d963bc2
chen jian <cjzainuli@gmail.com>
2015-12-16 19:22:11 +0800

    del file W

--- ferrari/urls.py
+++ ferrari/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from users import views as usersViews
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', usersViews.login, name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def login(request):
+    title = "DMP"
+    return render(request, 'users/head.html', {"title": title})
+
+
+# Testing
+def test(request):
+    return HttpResponse("hello users")
